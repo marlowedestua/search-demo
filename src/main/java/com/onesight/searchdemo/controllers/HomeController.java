2ade55683ab0241f7e1ef1e2680ca4fc8bd16cf2
marlowedestua <marlowe.destua@gmail.com>
2022-11-29 05:22:29 +0800

    Added init page

---
+++ src/main/java/com/onesight/searchdemo/controllers/HomeController.java
@@ -0,0 +1,21 @@
+package com.onesight.searchdemo.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class HomeController {
+
+    @GetMapping
+    @CrossOrigin(origins = {"*"})
+    public Map<String, String> index() {
+        Map<String, String> versionDetails = new HashMap<>();
+        versionDetails.put("application", "Search Demo");
+        versionDetails.put("version", "2.0.0");
+        return versionDetails;
+    }
+
+}
